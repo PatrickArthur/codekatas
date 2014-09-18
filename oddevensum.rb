9d377b7328136007c4b5fec6fb3ea879b7d56043
Patrick <pa@arthurfamily.ws>
2014-09-18 18:10:47 -0400

    automate

--- oddevensum.rb
+++ oddevensum.rb
@@ -1,15 +1,38 @@
-def oddevensum(array)
-  oddsum=0
-  evensum=0
-  odd, even = array.partition { |x| x % 2 == 1 }
-  odd.each do |x|
-    oddsum+=x
+
+def addnum
+  @numbers = []
+  puts 'Enter numbers you want to see the sum of odd and sum of even'
+  input = gets.chomp
+  while input != ""
+    @numbers << input.to_i
+    input = gets.chomp
   end
-  even.each do |y|
-    evensum+=y
-  end
-  puts "The sum of #{odd.length} odd numbers is #{oddsum}"
-  puts "The sum of #{even.length} even numbers is #{evensum}"
 end
 
-oddevensum([1,2,3,4,5,6,7,8,9,10,11])
+def partoddeven
+  @oddsum=0
+  @evensum=0
+  @odd, @even = @numbers.partition { |x| x % 2 == 1 }
+end
+
+def oddevensum
+  @odd.each do |x|
+    @oddsum+=x
+  end
+  @even.each do |y|
+    @evensum+=y
+  end
+end
+
+def report
+  puts "There are #{@odd.length} odd numbers and their sum is #{@oddsum}"
+  puts "There are  #{@even.length} even numbers and their sum is #{@evensum}"
+end
+
+addnum
+partoddeven
+oddevensum
+report
+
+
+
