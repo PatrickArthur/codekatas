5d47eec06a5734be3c8d328f5d17fb26264a03d2
Patrick <pa@arthurfamily.ws>
2015-04-29 22:31:30 -0400

    minor refactor

--- missing_number.rb
+++ missing_number.rb
@@ -1,7 +1,11 @@
 def missing_number(array)
   test = (array.first..array.last).to_a
   test2 = test-array
-  puts test2
+  if test2.length == 0
+    pust "No Missing Values"
+  else
+    puts "Missing values are #{test2}"
+  end
 end
 
-missing_number([1,2,9,10])
+missing_number([1,2,13])
